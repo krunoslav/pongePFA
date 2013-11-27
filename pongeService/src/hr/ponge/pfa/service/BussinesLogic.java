dee1fb90678a7897ecbad853498469e67a749a7f
Kruno <kruno.samardzic@orka.hr>
2013-11-27 12:28:13 +0100

    Work

---
+++ pongeService/src/hr/ponge/pfa/service/BussinesLogic.java
@@ -0,0 +1,21 @@
+package hr.ponge.pfa.service;
+
+import hr.ponge.pfa.PfaException;
+import hr.ponge.pfa.service.base.ErrorType;
+import hr.ponge.util.PfaSingleton;
+
+public abstract class BussinesLogic {
+
+	public ErrorType createError(String errorCode, String errorMessageKey,
+			String... params) {
+
+		ErrorType er = PfaSingleton.getReference()
+				.createObject(ErrorType.class);
+		er.setErrorCode(errorCode);
+		er.setErrorMessageKey(errorMessageKey);
+		er.setErrorParams(params);
+
+		return er;
+	}
+
+}
